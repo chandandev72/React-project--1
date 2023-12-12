e7c1aeaedbe7a6772c862f810388be23d455fc4f
chandan shutradhar <145096947+chandandev72@users.noreply.github.com>
2023-12-12 13:20:09 +0600

    as

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
+
+
+function App() {
+  
+
+  return (
+   <div>
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
+      
+   </div>
+  )
+}
+
+export default App
+
